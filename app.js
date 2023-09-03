37188e64680dbb6f276e15e5f2936b2f14ba8a5d
Aditya Gupta <adityanshu22@gmail.com>
2023-09-03 14:27:21 -0400

    Hello World server code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const http = require('http');
+
+const hostnameAdi = '127.0.0.1';
+const portNumber = 4000;
+
+const serverAdi = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World');
+});
+
+serverAdi.listen(portNumber, hostnameAdi, () => {
+    console.log(`Server is running at address http://${hostnameAdi}:${portNumber}/`);
+});
